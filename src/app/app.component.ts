da3b2f25d3f2284ba87e6c953bf285b69608347d
jeelnarola <narolajeel29@gmail.com>
2024-05-31 18:11:39 +0530

    FormArray

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,52 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Angular_FormArray';
+  @ViewChild('emailinputsub') emailinputsub:any
+  myForm!:FormGroup;
+  fild=['Frontend Coding','Backend Coding']
+  constructor(private fb:FormBuilder){
+  }
+  ngOnInit(): void {
+    this.myForm=this.fb.group({
+      name:new FormControl('',[Validators.required]),
+      email:new FormControl('',[Validators.required]),
+      emails:this.fb.array([]),
+      developer:new FormControl('',[Validators.required]),
+      skills:this.fb.array([])
+    })
+  }
+
+  get emails(){
+    return this.myForm.get('emails') as FormArray
+  }
+  get skills():FormArray{
+    return this.myForm.get('skills') as FormArray
+  }
+
+  emailcontroler(): FormControl {
+    return this.fb.control('', [Validators.required, Validators.email]); // Add email validator here
+  }
+ addEmail(){
+  this.emails.push(this.emailcontroler())
+ }
+
+ addSkill(){
+  const skill=this.fb.group({
+    skillname:new FormControl('')
+  })
+  this.skills.push(skill)
+ }
+ removeEmail(i:any){
+  this.emails.removeAt(i)
+ }
+ removeSkill(i:any){
+  this.skills.removeAt(i)
+ }
+ postFromArray(){
+  console.log("done",this.myForm.value);
+ }
+
+ get emailvalidation(){return this.myForm.get('email')}
+ get developervalidation(){return this.myForm.get('developer')}
+ get namevalidation(){return this.myForm.get('name')}
 }
